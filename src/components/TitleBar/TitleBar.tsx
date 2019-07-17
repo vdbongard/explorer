70e0a70e55a1a470339a331f8a3f8d988d07fbf4
vdbongard <daniel@vdbongard.com>
2019-07-17 19:48:54 +0200

    Added min, max and close buttons

--- src/components/TitleBar/TitleBar.tsx
+++ src/components/TitleBar/TitleBar.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement, useState } from 'react';
+import React, { FC, ReactElement, ReactNode, useState } from 'react';
 import './TitleBar.scss';
 import Tab from '../Tab/Tab';
 
@@ -6,21 +6,30 @@ const tabs = ['Tab 1', 'Tab 2'];
 
 const TopBar: FC = (): ReactElement => {
   const [selectedTab, setSelectedTab] = useState(0);
+  const [maximized, setMaximized] = useState(false);
 
-  const Tabs = tabs.map((name, index) => (
-    <Tab
-      name={name}
-      index={index}
-      key={index}
-      selected={index === selectedTab}
-      onClick={(i): void => setSelectedTab(i)}
-    />
-  ));
+  const Tabs = tabs.map(
+    (name, index): ReactNode => (
+      <Tab
+        name={name}
+        index={index}
+        key={index}
+        selected={index === selectedTab}
+        onClick={(i): void => setSelectedTab(i)}
+      />
+    )
+  );
+
+  const toggleMaximize = (): void => setMaximized(!maximized);
 
   return (
     <div className="TopBar">
       <div className="tabs">{Tabs}</div>
-      <div className="actions" />
+      <div className="actions">
+        <button className="minimize" />
+        <button className={maximized ? 'restore' : 'maximize'} onClick={toggleMaximize} />
+        <button className="close" />
+      </div>
     </div>
   );
 };
