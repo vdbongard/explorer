656edc91659bde283f5b5807162ac1f7de039511
vdbongard <daniel@vdbongard.com>
2019-07-17 16:39:43 +0200

    Import reboot

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { FC, ReactElement } from 'react';
 import './App.scss';
+import './styles/_reboot.scss';
 import Explorer from './components/Explorer/Explorer';
 
 const App: FC = (): ReactElement => {
